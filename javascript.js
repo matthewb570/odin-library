8cca849ad912ebb3f72ceb6f1d235e98a51a16b0
Matthew Burgess <boog397@gmail.com>
2024-05-18 13:35:16 -0500

    Add library array

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
+const myLibrary = [];
+
 function Book(title, author, numPages, haveRead) {
     this.title = title;
     this.author = author;
     this.numPages = numPages;
     this.haveRead = haveRead;
-    this.info = function() {
+    this.info = function() { // TODO: Add to prototype
         return this.title + " by " + this.author + ", " + this.numPages + " pages, " + (this.haveRead ? "already read" : "not read yet");
     }
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+function addBookToLibrary(title, author, numPages, haveRead) {
+    myLibrary.push(new Book(title, author, numPages, haveRead));
+}
+
+// let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+// console.log(theHobbit.info());
