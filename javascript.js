4e07ca5c6b87aa18fa7a7cbf7fc7ab96bc9c9ca9
Matthew Burgess <boog397@gmail.com>
2024-05-18 23:59:07 -0500

    Add ability to toggle read status for each book card

--- javascript.js
+++ javascript.js
@@ -53,12 +53,15 @@ function displayBooks() {
         pageCount.classList.add("page-count");
         pageCount.textContent = `${book.numPages} pages`;
 
-        let readStatus = document.createElement("div");
-        readStatus.classList.add("read-status");
-        readStatus.textContent = book.haveRead ? "Read" : "Not Read";
+        let btnReadStatus = document.createElement("button");
+        btnReadStatus.classList.add("read-status");
+        btnReadStatus.type = "button";
+        btnReadStatus.textContent = book.haveRead ? "Read" : "Not Read";
+        btnReadStatus.addEventListener("click", handleReadStatusChange);
+        btnReadStatus.setAttribute("index", bookIndex);
 
         additionalInfo.appendChild(pageCount);
-        additionalInfo.appendChild(readStatus);
+        additionalInfo.appendChild(btnReadStatus);
 
         card.appendChild(title);
         card.appendChild(btnDelete);
@@ -73,6 +76,12 @@ function displayBooks() {
 function handleDelete(event) {
     myLibrary.splice(event.target.getAttribute("index"), 1);
     displayBooks();
+}
+
+function handleReadStatusChange(event) {
+    let book = myLibrary[event.target.getAttribute("index")];
+    book.haveRead = !book.haveRead;
+    event.target.textContent = book.haveRead ? "Read" : "Not Read";
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "A story about hobbits.", 295, false);
